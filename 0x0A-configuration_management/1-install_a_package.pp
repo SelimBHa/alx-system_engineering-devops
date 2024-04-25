3ff253d181c8f6b368e95c6fd83d43ee69f3cc29
SelimBHa <141486892+SelimBHa@users.noreply.github.com>
2024-04-25 21:17:16 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,5 @@
-# File: puppet_manifest.pp
-
-# Puppet manifest to install Flask from pip3
-class { 'python':
-  version => 'present',
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => 'pip3 install Flask==2.1.0',
-  path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-  unless      => 'pip3 show Flask | grep -q "Version: 2.1.0"',
-  timeout     => 300,
-  logoutput   => true,
-}
-
-# Ensure that pip3 has been installed before trying to install Flask
-Package['python3-pip'] -> Exec['install_flask']
